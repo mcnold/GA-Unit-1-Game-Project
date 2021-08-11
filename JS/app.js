17cd364d2af9780ec682d9ab81f7b51a5ad07f16
mcnold <michele.godleske@gmail.com>
2021-08-10 20:17:25 -0400

    Initial commit for wireframes

--- JS/app.js
+++ JS/app.js
@@ -7,18 +7,47 @@
 const canvas= document.querySelector("#canvas")
 canvas.width = innerWidth
 canvas.height = innerHeight
-const context = canvas.getContext('2d')
-console.log(canvas)
-//Set up timer for animation refresh.
-//const framesPerSecond = 30
-//etInterval(update, 1000 / framesPerSecond) 
+const ctx = canvas.getContext('2d')
+ctx.fillStyle = "#861c23"
+ctx.fillRect(0,0,canvas.width, canvas.height);
+const photographer = document.querySelector('#photographer')
+//ctx.scale(-1,1)
+ctx.drawImage(photographer,1200,300,200,200)
+const starlet1 = document.querySelector('#starlet1')
+//ctx.scale(1,1)
+ctx.drawImage(starlet1,500,50,300,300)
+const starlet2 = document.querySelector('#starlet2')
+ctx.drawImage(starlet2,200,100,300,300)
+//const securityGuard = document.querySelector('#securityGuard')
+//ctx.drawImage(securityGuard, 90,10,50,50)
 
+//console.log(ctx)
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //Global scope for game
 let score = 0
-let lives = 3
+let soundOn = false;
+let musicOn = false;
+const lives = 3
+const framesPerSecond = 30
+const friction = 0.5 //adds friction to movement of the photographer
+const photographerLosingLifeDuration = 0.1
+const handcuffsDuration = 0.3
+const photographerTurnSpeed = 360
+const flashBulbDistance = 0.3
+const flashBulbDuration = 0.1
+const flashBulbMax = 10
+const flashBulbSpeed = 200
+const celebrityNumber = 1
+const celebritySize = 100
+const celebritySpeed = 50
+const celebrityLargePts = 20
+const celebrityMediumPts = 50
+const celebritySmallPts = 100
 
-class CameraMan {
+//setInterval(update, 1000/ framesPerSecond)
+    
+ 
+class Photographer {
     constructor() {
         this.width=width
         this.height=height
